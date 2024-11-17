728cff0ce3c43bcefc4e975f0335e2e576071de0
joshua <iuttn123@gmail.com>
2024-11-17 13:06:53 +0900

    첫리액트

---
+++ src/Button/BlueButton.jsx
@@ -0,0 +1,12 @@
+const BlueButton = () => {
+  const css = {
+    backgroundColor: "#2563EB",
+    padding: "12px 35px;",
+    border: "1px solid #2563EB",
+    color: "white",
+    fontSize: "14px",
+    borderRadius: "40px",
+  };
+  return <button style={css}>Button</button>;
+};
+export default BlueButton;
---
+++ src/Button/DeepBlueButton.jsx
@@ -0,0 +1,12 @@
+const DeepBlueButton = () => {
+  const css = {
+    backgroundColor: "#1D4ED8",
+    padding: "12px 35px;",
+    border: "1px solid #1D4ED8",
+    color: "white",
+    fontSize: "14px",
+    borderRadius: "40px",
+  };
+  return <button style={css}>Button</button>;
+};
+export default DeepBlueButton;
